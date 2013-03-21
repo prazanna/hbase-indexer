388a6dcbd21875d97f7a086ca64d20579465382e
Bruno Dumon <bruno@ngdata.com>
2013-03-21 12:04:43 +0100

    Make DemoUserIngester suited for massive uploading
    Added options to use mulitple threads & multi-put.

--- hbase-indexer-demo/src/main/java/com/ngdata/hbaseindexer/demo/DemoUserIngester.java
+++ hbase-indexer-demo/src/main/java/com/ngdata/hbaseindexer/demo/DemoUserIngester.java
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package com.ngdata.hbaseindexer.demo;
 
 import java.io.BufferedReader;
@@ -23,7 +21,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 
+import joptsimple.ArgumentAcceptingOptionSpec;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
@@ -33,44 +35,110 @@ import org.apache.hadoop.hbase.util.Bytes;
 public class DemoUserIngester {
     private List<String> names;
     private List<String> domains;
+    private AtomicLong totalPuts = new AtomicLong();
+
+    private static final byte[] infoCf = Bytes.toBytes("info");
+
+    // column qualifiers
+    private static final byte[] firstNameCq = Bytes.toBytes("firstname");
+    private static final byte[] lastNameCq = Bytes.toBytes("lastname");
+    private static final byte[] emailCq = Bytes.toBytes("email");
+    private static final byte[] ageCq = Bytes.toBytes("age");
 
     public static void main(String[] args) throws Exception {
-        new DemoUserIngester().run();
+        new DemoUserIngester().run(args);
     }
 
-    public void run() throws Exception {
+    public void run(String[] args) throws Exception {
+        OptionParser parser =  new OptionParser();
+
+        ArgumentAcceptingOptionSpec<Integer> threadsOption =
+                parser.accepts("threads", "number of concurrent threads")
+                        .withRequiredArg()
+                        .ofType(Integer.class)
+                        .defaultsTo(1);
+
+        ArgumentAcceptingOptionSpec<Integer> batchSizeOption =
+                parser.accepts("batchsize", "size of multi-puts done to hbase")
+                        .withRequiredArg()
+                        .ofType(Integer.class)
+                        .defaultsTo(1);
+
+        OptionSet options = parser.parse(args);
+
+        final int threads = threadsOption.value(options);
+        final int batchSize = batchSizeOption.value(options);
+
         Configuration conf = HBaseConfiguration.create();
 
         DemoSchema.createSchema(conf);
 
-        final byte[] infoCf = Bytes.toBytes("info");
-
-        // column qualifiers
-        final byte[] firstNameCq = Bytes.toBytes("firstname");
-        final byte[] lastNameCq = Bytes.toBytes("lastname");
-        final byte[] emailCq = Bytes.toBytes("email");
-        final byte[] ageCq = Bytes.toBytes("age");
-        
         loadData();
 
-        HTable htable = new HTable(conf, DemoSchema.USER_TABLE);
+        for (int i = 0; i < threads; i++) {
+            Thread thread = new Thread(new Putter("thread" + i, conf, batchSize));
+            thread.start();
+        }
 
-        while (true) {
-            byte[] rowkey = Bytes.toBytes(UUID.randomUUID().toString());
-            Put put = new Put(rowkey);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        System.out.printf("[threads: %s, batch size: %s] Total puts until now %s\n",
+                                threads, batchSize, totalPuts.get());
+                        Thread.sleep(2000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
-            String firstName = pickName();
-            String lastName = pickName();
-            String email = firstName.toLowerCase() + "@" + pickDomain();
-            int age = (int) Math.ceil(Math.random() * 100);
+    }
 
-            put.add(infoCf, firstNameCq, Bytes.toBytes(firstName));
-            put.add(infoCf, lastNameCq, Bytes.toBytes(lastName));
-            put.add(infoCf, emailCq, Bytes.toBytes(email));
-            put.add(infoCf, ageCq, Bytes.toBytes(age));
+    private class Putter implements Runnable {
+        private String name;
+        private Configuration conf;
+        private int batchSize;
 
-            htable.put(put);
-            System.out.println("Added row " + Bytes.toString(rowkey));
+        public Putter(String name, Configuration conf, int batchSize) {
+            this.name = name;
+            this.conf = conf;
+            this.batchSize = batchSize;
+        }
+
+        @Override
+        public void run() {
+            try {
+                HTable htable = new HTable(conf, DemoSchema.USER_TABLE);
+
+                while (true) {
+                    List<Put> puts = new ArrayList<Put>();
+                    for (int i = 0; i < batchSize; i++) {
+                        byte[] rowkey = Bytes.toBytes(UUID.randomUUID().toString());
+                        Put put = new Put(rowkey);
+
+                        String firstName = pickName();
+                        String lastName = pickName();
+                        String email = firstName.toLowerCase() + "@" + pickDomain();
+                        int age = (int) Math.ceil(Math.random() * 100);
+
+                        put.add(infoCf, firstNameCq, Bytes.toBytes(firstName));
+                        put.add(infoCf, lastNameCq, Bytes.toBytes(lastName));
+                        put.add(infoCf, emailCq, Bytes.toBytes(email));
+                        put.add(infoCf, ageCq, Bytes.toBytes(age));
+
+                        puts.add(put);
+                    }
+
+                    htable.put(puts);
+                    totalPuts.addAndGet(puts.size());
+                }
+            } catch (Throwable t) {
+                System.err.println("Thread " + name + " dying because of an error");
+                t.printStackTrace(System.err);
+            }
         }
     }
 
